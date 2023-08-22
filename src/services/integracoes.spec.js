ebbc9522cbdc9e6104cb894b20e0fd000b72a141
Vitor Soares <vitorsoaresf@gmail.com>
2023-08-22 07:46:55 -0300

    feat: implements unit test api mock

--- src/services/integracoes.spec.js
+++ src/services/integracoes.spec.js
@@ -23,12 +23,28 @@ const mockRequisicao = (retorno) => {
   });
 };
 
+const mockRequisicaoErro = () => {
+  return new Promise((_, rej) => {
+    setTimeout(() => {
+      rej();
+    }, 200);
+  });
+};
+
 describe('Requisicoes para API', () => {
   test('Deve retornar uma lista de transacoes', async () => {
     api.get.mockImplementation(() => mockRequisicao(mockTransacao));
     const transacoes = await buscaTransacoes(mockTransacao);
 
     expect(transacoes).toEqual(mockTransacao);
+    expect(api.get).toHaveBeenCalledWith('/transacoes');
+  });
+
+  test('Deve retornar uma lista vazia quando a requisicao falhar', async () => {
+    api.get.mockImplementation(() => mockRequisicaoErro());
+    const transacoes = await buscaTransacoes(mockTransacao);
+
+    expect(transacoes).toEqual([]);
     expect(api.get).toHaveBeenCalledWith('/transacoes');
   });
 });
